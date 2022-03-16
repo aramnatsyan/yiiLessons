3489603221f59abd56d3b1975e4c3308ca6a30bc
Ara <aramnatsyan018@gmail.com>
2022-03-16 20:52:48 +0400

    creating layouts ang using

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -15,7 +15,19 @@ class PostController extends AppController
             'act2' => 'test2',
             'act3' => 'test2'
         ];
-        
+
         return $this->render('test', compact('arr'));
+    }
+
+    public function actionShow()
+    {
+        $arr = [
+            'act1' => 'test1',
+            'act2' => 'test2',
+            'act3' => 'test2'
+        ];
+        $this->layout = 'basic';
+
+        return $this->render('show', compact('arr'));
     }
 }
